2f9fb8862658fbc4c4ea762a95ba97bcd786aa50
Lucas <lquintanilla@a1-tec.com>
2020-12-01 01:20:37 -0300

    Changed repository location

--- OriginBank.Models/Card.cs
+++ OriginBank.Models/Card.cs
@@ -10,9 +10,9 @@ namespace OriginBank.Models
         public int Id { get; set; }
 
         [Required]
-        //[RegularExpression(@"^\d{4}(-\d{4}){3}$", ErrorMessage = "The card number must have the following format: 1111-1111-1111-1111")]
-        //[StringLength(19, MinimumLength = 19)]
-        [DataType(DataType.CreditCard)]
+        [RegularExpression(@"^\d{4}(-\d{4}){3}$", ErrorMessage = "The card number must have the following format: 1111-1111-1111-1111")]
+        [StringLength(19, MinimumLength = 19)]
+        //[DataType(DataType.CreditCard)]
         public string Number { get; set; }
 
         [Range(1000, 9999)]
